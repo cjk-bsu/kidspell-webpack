3cb5050d23e35f34eca9f93f6ae0e15fb7644a49
Carson Keller <carsonkeller@u.boisestate.edu>
2024-07-13 20:09:30 -0600

    Initial commit; enabled script injection and canvas annotation

---
+++ src/js/docs_enable_canvas_annotation.js
@@ -0,0 +1,4 @@
+const VALID_EDITING_EXT_ID = "ocnjkingnoccghefojojhknfnpmegmnc";
+// const VALID_EDITING_EXT_ID = chrome.runtime.id;
+
+(() => { window._docs_annotate_canvas_by_ext = VALID_EDITING_EXT_ID; })();
